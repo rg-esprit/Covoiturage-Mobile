1a15405a0581f5e205e125fb631b1e7be5c9b424
rg-esprit <guembrk.raslen@esprit.tn>
2025-01-18 06:30:45 +0100

    feat: implement login functionality and add LoginScreen component

--- App.js
+++ App.js
@@ -1,9 +1,27 @@
-import { StyleSheet, Text, SafeAreaView, Platform, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Platform,
+  View,
+  Alert,
+} from "react-native";
+import { useState } from "react";
+import LoginScreen from "./app/screens/LoginScreen";
 
 export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  function handleloginpressed(loginInfo) {
+    Alert.alert("Login Info", loginInfo);
+    setIsLoggedIn(true);
+  }
   return (
     <SafeAreaView style={styles.container}>
-      <View></View>
+      {isLoggedIn ? (
+        <Text>Welcome back!</Text>
+      ) : (
+        <LoginScreen loginpressed={handleloginpressed} />
+      )}
     </SafeAreaView>
   );
 }
---
+++ app/screens/LoginScreen.js
@@ -0,0 +1,218 @@
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Platform,
+  View,
+  TextInput,
+  Pressable,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { useState } from "react";
+
+export default function LoginScreen({ loginpressed }) {
+  const [AlreadyRegistred, setAlreadyRegistred] = useState(false);
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        {/* <View style={styles.header}>
+          <Pressable style={styles.closeButton}>
+            <Ionicons name="close" size={24} color="white" />
+          </Pressable>
+        </View> */}
+        {AlreadyRegistred ? (
+          <Text style={styles.title}>Sign in to your account</Text>
+        ) : (
+          <Text style={styles.title}>Create a new account</Text>
+        )}
+        <View style={styles.inputContainer}>
+          <View style={styles.inputWrapper}>
+            <TextInput
+              placeholder="Email"
+              placeholderTextColor="#9cabba"
+              style={styles.input}
+            />
+            <Ionicons
+              name="mail"
+              size={24}
+              color="#9cabba"
+              style={styles.inputIcon}
+            />
+          </View>
+        </View>
+        <View style={styles.inputContainer}>
+          <View style={styles.inputWrapper}>
+            <TextInput
+              placeholder="Password"
+              placeholderTextColor="#9cabba"
+              secureTextEntry
+              style={styles.input}
+            />
+            <Ionicons
+              name="key"
+              size={24}
+              color="#9cabba"
+              style={styles.inputIcon}
+            />
+          </View>
+        </View>
+
+        {!AlreadyRegistred && (
+          <View style={styles.inputContainer}>
+            <View style={styles.inputWrapper}>
+              <TextInput
+                placeholder="Confirm Password"
+                placeholderTextColor="#9cabba"
+                secureTextEntry
+                style={styles.input}
+              />
+              <Ionicons
+                name="key"
+                size={24}
+                color="#9cabba"
+                style={styles.inputIcon}
+              />
+            </View>
+          </View>
+        )}
+        {AlreadyRegistred ? (
+          <View style={styles.buttonContainer}>
+            <Pressable
+              onPress={() => loginpressed("information of login will be here")}
+              style={styles.loginButton}
+            >
+              <Text style={styles.loginButtonText}>Login</Text>
+            </Pressable>
+          </View>
+        ) : (
+          <View style={styles.buttonContainer}>
+            <Pressable
+              onPress={() => loginpressed("information of login will be here")}
+              style={styles.loginButton}
+            >
+              <Text style={styles.loginButtonText}>SignUp</Text>
+            </Pressable>
+          </View>
+        )}
+        {AlreadyRegistred ? (
+          <>
+            <Pressable>
+              <Text style={styles.linkText}>Forgot password?</Text>
+            </Pressable>
+            <Pressable onPress={() => setAlreadyRegistred(false)}>
+              <Text style={styles.linkText}>
+                Don't have an account? Sign up.
+              </Text>
+            </Pressable>
+          </>
+        ) : (
+          <Pressable onPress={() => setAlreadyRegistred(true)}>
+            <Text style={styles.linkText}>
+              You already have an account? Log in!
+            </Text>
+          </Pressable>
+        )}
+        <Text style={styles.orText}>Or continue with</Text>
+        <View style={styles.socialButtonsContainer}>
+          <Pressable style={styles.socialButton}>
+            <Text style={styles.socialButtonText}>Google</Text>
+          </Pressable>
+          <Pressable style={styles.socialButton}>
+            <Text style={styles.socialButtonText}>Facebook</Text>
+          </Pressable>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    paddingBottom: 8,
+  },
+  closeButton: {
+    padding: 8,
+  },
+  title: {
+    color: "white",
+    fontSize: 22,
+    fontWeight: "bold",
+    textAlign: "center",
+    paddingVertical: 20,
+  },
+  inputContainer: {
+    marginVertical: 12,
+  },
+  inputWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#283039",
+    borderRadius: 12,
+  },
+  input: {
+    flex: 1,
+    height: 56,
+    color: "white",
+    padding: 16,
+    fontSize: 16,
+  },
+  inputIcon: {
+    paddingRight: 16,
+  },
+  buttonContainer: {
+    marginVertical: 12,
+  },
+  loginButton: {
+    backgroundColor: "#0d80f2",
+    height: 48,
+    borderRadius: 12,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  loginButtonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  linkText: {
+    color: "#9cabba",
+    fontSize: 14,
+    textDecorationLine: "underline",
+    marginVertical: 4,
+  },
+  orText: {
+    color: "#9cabba",
+    fontSize: 14,
+    textAlign: "center",
+    marginTop: 50,
+  },
+  socialButtonsContainer: {
+    flexDirection: "row",
+    gap: 12,
+    marginTop: 12,
+    justifyContent: "center",
+  },
+  socialButton: {
+    backgroundColor: "#283039",
+    height: 40,
+    paddingHorizontal: 16,
+    borderRadius: 12,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  socialButtonText: {
+    color: "white",
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+});
